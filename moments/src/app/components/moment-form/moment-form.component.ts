0d36c7d6421c7c3ce0d9231010495073a924729e
heberGustavo <heber.gbarbosa@hotmail.com>
2025-09-08 08:33:06 -0300

    Validações com Reactive Forms

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,11 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
-export class MomentFormComponent {
-  @Input() btnText!: string; 
+export class MomentFormComponent implements OnInit {
+  @Input() btnText!: string;
+  momentForm!: FormGroup;
+  
+  constructor() {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid)
+      return;
+    
+    console.log("envio form");
+  }
 }
