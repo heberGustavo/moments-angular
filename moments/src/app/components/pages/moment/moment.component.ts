3c1e547a7d70865a53476fb6f54f525def341a3e
heberGustavo <heber.gbarbosa@hotmail.com>
2025-09-16 14:48:42 -0300

    Implementar comentarios

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Moment } from '../../../interface/Moments';
-import { MomentService } from '../../../services/moment.service';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../../environment/environment';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { Moment } from '../../../interface/Moments';
+import { MomentService } from '../../../services/moment.service';
+import { environment } from '../../../../environment/environment';
 import { MessageService } from '../../../services/message.service';
+import { CommentService } from '../../../services/comment.service';
+import { Comment } from '../../../interface/Comment';
 
 @Component({
   selector: 'app-moment',
-  imports: [CommonModule, RouterLink, FontAwesomeModule],
+  imports: [CommonModule, RouterLink, FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './moment.component.html',
   styleUrl: './moment.component.css',
 })
@@ -21,11 +32,14 @@ export class MomentComponent implements OnInit {
   faTimes = faTimes;
   faEdit = faEdit;
 
+  commentForm!: FormGroup;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private momentService: MomentService,
     private messageService: MessageService,
+    private commentService: CommentService
   ) {}
 
   ngOnInit(): void {
@@ -34,13 +48,45 @@ export class MomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((itemMoment) => (this.moment = itemMoment.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    });
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  get username() {
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id: number) {
     await this.momentService.deleteMoment(id).subscribe();
 
-    this.messageService.add("Momento excluído com sucesso!");
+    this.messageService.add('Momento excluído com sucesso!');
 
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
+  }
+
+  async onSubmit() {
+    if (this.commentForm.invalid) {
+      this.commentForm.markAllAsTouched(); // força exibir os erros
+      return;
+    }
+
+    const data: Comment = this.commentForm.value;
+    data.momentId = Number(this.moment!.id);
+
+    await this.commentService
+      .createComment(data)
+      .subscribe((comment) => this.moment!.comments!.push(comment.data));
+
+    this.messageService.add("Comentário adicionado!");
+
+    //Limpa os dados
+    this.commentForm.reset();
   }
 }
--- moments/src/app/services/comment.service.ts
+++ moments/src/app/services/comment.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environment/environment';
+import { Comment } from '../interface/Comment';
+import { Response } from '../interface/Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`
+
+  constructor(private http: HttpClient) { }
+
+  createComment(data: Comment): Observable<Response<Comment>>{
+    const url = `${this.apiUrl}/${data.momentId}/comments`;
+    return this.http.post<Response<Comment>>(url, data);
+  }
 }
